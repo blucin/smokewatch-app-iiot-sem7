b186ec49be72813cf0e84e3ccd401162d91fdf8e
akshar <glaivesharp@gmail.com>
2023-09-23 18:24:07 +0530

    Initial boilerplate

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { AppRegistry } from "react-native";
+import { PaperProvider } from "react-native-paper";
+import { StyleSheet, Text, View } from "react-native";
+import {
+	NavigationContainer,
+	DefaultTheme,
+	DarkTheme,
+} from "@react-navigation/native";
+import BottomNav from "./components/BottomNav";
+
+// theme
+import { useMaterial3Theme } from "@pchmn/expo-material3-theme";
+import { useColorScheme } from "react-native";
+import {
+	MD3DarkTheme,
+	MD3LightTheme,
+	adaptNavigationTheme,
+} from "react-native-paper";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	// react native paper theme
+	const colorScheme = useColorScheme();
+	const { theme } = useMaterial3Theme();
+	const paperTheme =
+		colorScheme === "dark"
+			? { ...MD3DarkTheme, colors: theme.dark }
+			: { ...MD3LightTheme, colors: theme.light };
+
+	// to sync paper theme with react navigation theme
+	const { LightTheme, DarkTheme } = adaptNavigationTheme({
+		reactNavigationLight: DefaultTheme,
+		materialLight: MD3LightTheme,
+		reactNavigationDark: DarkTheme,
+		materialDark: MD3DarkTheme,
+	});
+  const navigationTheme = colorScheme === 'dark' ? DarkTheme : LightTheme;
+
+	return (
+		<PaperProvider theme={paperTheme}>
+			<NavigationContainer
+				theme={navigationTheme}
+			>
+				<StatusBar style="auto" />
+				<BottomNav />
+			</NavigationContainer>
+		</PaperProvider>
+	);
 }
 
+AppRegistry.registerComponent("eiot", () => Main);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
