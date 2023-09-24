a306dbe91bc98cafc806a0e5b352455a0089edcc
akshar <glaivesharp@gmail.com>
2023-09-24 18:42:12 +0530

    Initial Graph and Settings page

--- routes/Settings.jsx
+++ routes/Settings.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { SafeAreaView, View, StyleSheet } from "react-native";
 import { Appbar, useTheme, Text } from "react-native-paper";
+import { TextInput, FAB } from "react-native-paper";
 
 export default function Settings() {
-  const theme = useTheme();
+	const theme = useTheme();
+	const [email, setEmail] = React.useState("test@gmail.com");
+	const [password, setPassword] = React.useState("password123");
+
 	return (
 		<SafeAreaView style={[styles.container]}>
 			<Appbar.Header
@@ -11,7 +15,29 @@ export default function Settings() {
 			>
 				<Appbar.Content title="Settings" />
 			</Appbar.Header>
-			<Text>Settings</Text>
+			<View style={[styles.sub_container]}>
+				<Text
+					variant="headlineSmall"
+					style={{ color: theme.colors.primary }}
+				>
+					Firebase Auth Cred
+				</Text>
+				<TextInput
+					label="Email"
+					value={email}
+					onChangeText={(email) => setEmail(email)}
+				/>
+				<TextInput
+					label="Password"
+					value={password}
+					onChangeText={(password) => setPassword(password)}
+				/>
+			</View>
+			<FAB
+				icon="content-save-outline"
+				style={styles.fab}
+				onPress={() => {}}
+			/>
 		</SafeAreaView>
 	);
 }
@@ -21,8 +47,15 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 	sub_container: {
-		paddingLeft: 15,
-		paddingRight: 15,
+		flex: 1,
+		justifyContent: "center",
+		padding: 15,
 		gap: 20,
 	},
+	fab: {
+    position: 'absolute',
+    margin: 30,
+    right: 0,
+    bottom: 0,
+  },
 });
